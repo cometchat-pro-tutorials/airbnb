d5be880e9cc511541e0f666f4c56986246abbb71
hieptl <hieptl.developer@gmail.com>
2022-01-28 14:47:21 +0700

    add clear form function

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -26,7 +26,7 @@ const Search = () => {
     if (keywords) {
       storageService.save({ key: STORAGE_KEYS.KEYWORD, payload: keywords });
       searchOrReload();
-      searchRef.current.value = '';
+      clearForm();
     }
   };
 
@@ -38,6 +38,12 @@ const Search = () => {
       uiService.showLightHeader();
       routeService.navigate({ route: ROUTE.SEARCH, push: history.push });
     }
+  };
+
+  const clearForm = () => {
+    searchRef.current.value = '';
+    setStartDate(null);
+    setEndDate(null);
   };
 
   if (!user) return <></>;
@@ -52,11 +58,11 @@ const Search = () => {
           </div>
           <div className="search__form-start-date search__form-element">
             <span>Start Date</span>
-            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)}/>
+            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
           </div>
           <div className="search__form-end-date search__form-element">
             <span>End Date</span>
-            <DatePicker selected={endDate} onChange={(date) => setEndDate(date)}/>
+            <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} />
           </div>
           <div className="search__icon" onClick={searchHouses}>
             <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{ display: 'block', fill: 'white', width: '12px', height: '12px', stroke: 'white', strokeWidth: '5.3333', overflow: 'visible' }}><g fill="none"><path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path></g></svg>
